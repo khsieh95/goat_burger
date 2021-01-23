daf6f0124f3e8e9f02a87f0216af02b6089a1024
Kevin Hsieh <khsieh95@gmail.com>
2021-01-23 15:19:36 -0800

    created all middleware files and starter code

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (name, devour) VALUES ('Plain', true);
+INSERT INTO burgers (name, devour) VALUES ('Krabby Patty', true);
+INSERT INTO burgers (name, devour) VALUES ('Krabby Patty with pickles', true);
+INSERT INTO burgers (name) VALUES ('Mushroom swiss');
